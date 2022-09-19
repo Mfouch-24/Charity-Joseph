2ec25efdd82e733e70c3f8c5f173d71665bca9b5
Matthew <mfouch5133@stu.d214.org>
2022-09-19 10:24:47 -0500

    Initial Commit

---
+++ Charity Joseph/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Charity Joseph
+//
+//  Created by Matthew J. Fouch on 9/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Charity JosephUITests/Charity_JosephUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Charity_JosephUITestsLaunchTests.swift
+//  Charity JosephUITests
+//
+//  Created by Matthew J. Fouch on 9/15/22.
+//
+
+import XCTest
+
+final class Charity_JosephUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
